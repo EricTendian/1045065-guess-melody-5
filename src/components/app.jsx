e193bdc58ffb9a875a124215d3b3881abb661685
Eric Tendian <erictendian@gmail.com>
2020-10-05 22:33:03 -0500

    Completing task 1 in module 2: creating routing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,38 @@
 import React from "react";
-import Welcome from "./welcome";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 
+import Welcome from "./welcome";
+import Login from "./login";
+import Result from "./result";
+import Lose from "./lose";
+import Artist from "./artist";
+import Genre from "./genre";
+
 const App = (props) => {
-  return <Welcome errors={props.errors} />;
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Welcome errors={props.errors} />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/result">
+        <Result />
+      </Route>
+      <Route path="/lose">
+        <Lose />
+      </Route>
+      <Route path="/dev-artist">
+        <Artist />
+      </Route>
+      <Route path="/dev-genre">
+        <Genre />
+      </Route>
+    </Switch>
+  </BrowserRouter>;
+
 };
 
 App.propTypes = {
