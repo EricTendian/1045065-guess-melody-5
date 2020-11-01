b92fb2b8b744bdaca73f85f2731eeafc9d8591cc
Eric Tendian <erictendian@gmail.com>
2020-11-01 16:23:59 -0600

    Responding to PR feedback

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/app";
 import questions from "./mocks/questions";
 
-const errors = 3;
+const ERRORS = 3;
 
 ReactDOM.render(
-    <App errors={errors} questions={questions} />,
+    <App errors={ERRORS} questions={questions} />,
     document.getElementById(`root`)
 );
